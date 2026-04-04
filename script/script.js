3bb6ffc681995b79015b65b4bc0e6bdccb209fd3
jubayer50 <aujubayer1@gmail.com>
2026-04-05 01:35:01 +0600

    modal and spinning function applied

--- script/script.js
+++ script/script.js
@@ -23,7 +23,19 @@ const displayLevels = (loadData) => {
   }
 };
 
+const manageSpinning = (status) => {
+  if (status === true) {
+    document.getElementById("spinning").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("spinning").classList.add("hidden");
+    document.getElementById("word-container").classList.remove("hidden");
+  }
+};
+
 const loadLevelWorld = (id) => {
+  manageSpinning(true);
+
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
 
   fetch(url)
@@ -58,11 +70,13 @@ const displayLevelWord = (words) => {
               <p class="text-[#79716B] font-ban">
                 এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
               </p>
-              <h2 class="text-[#292524] font-bold text-4xl">
+              <h2 class="text-[#292524] font-bold text-3xl md:text-4xl">
                 নেক্সট Lesson এ যান
               </h2>
             </div>
     `;
+
+    manageSpinning(false);
     return;
   }
 
@@ -79,7 +93,7 @@ const displayLevelWord = (words) => {
                 </h2>
               </div>
 
-              <div class="mt-11 flex justify-between items-center">
+              <div onclick="leadWordDetails(${word.id})" class="mt-11 flex justify-between items-center">
                 <button class="btn">
                   <i class="fa-solid fa-circle-info"></i>
                 </button>
@@ -91,6 +105,71 @@ const displayLevelWord = (words) => {
     `;
 
     wordContainer.append(wordDiv);
+
+    manageSpinning(false);
   });
   // console.log(words);
+};
+
+const leadWordDetails = async (id) => {
+  // console.log(id);
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  displayWordDetail(data.data);
+};
+
+const displayWordDetail = (word) => {
+  // console.log(word);
+
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.innerHTML = "";
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+              <h2 class="text-[28px] md:text-[32px] font-semibold">
+                ${word.word} (<i class="fa-solid fa-microphone"></i>:${word.meaning})
+              </h2>
+
+              <div class="mt-4 md:mt-7">
+                <p class="text-[28px] md:text-[30px] font-semibold">Meaning</p>
+                <p
+                  class="text-[19px] md:text-[22px] font-ban font-medium mt-1.5"
+                >
+                  ${word.meaning}
+                </p>
+              </div>
+
+              <div class="mt-4 md:mt-7">
+                <h3 class="text-[28px] md:text-3xl font-semibold">Example</h3>
+                <p class="text-[16px] md:text-lg mt-1 font-medium">
+                  ${word.sentence}
+                </p>
+              </div>
+
+              <div class="mt-7">
+                <h4 class="text-[16px] md:text-lg font-medium font-ban">
+                  সমার্থক শব্দ গুলো
+                </h4>
+                <div class='mt-2 flex flex-wrap gap-2'>${createElement(word.synonyms)}</div>
+              </div>
+
+              <div class="mt-4 md:mt-5">
+                <button
+                  class="bg-[#422AD5] px-5 py-1.5 rounded-md text-white font-medium text-xl cursor-pointer"
+                >
+                  Complete Learning
+                </button>
+              </div>
+  `;
+
+  modalContainer.append(div);
+
+  document.getElementById("Word_detail_modal").showModal();
+};
+
+const createElement = (array) => {
+  const htmlElement = array.map((el) => ` <span class='btn'>${el}</span>`);
+  return htmlElement.join(" ");
 };
